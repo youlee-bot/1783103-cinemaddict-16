eda2b9c627ae6cc2c74dc72aaebf023612f5b9ef
you lee <letsgosmoke@gmail.com>
2021-11-24 20:52:23 +0800

    Установил и настроил babel

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: false
+  }
+};
