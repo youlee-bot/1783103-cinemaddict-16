ee5aa130501b5dd2d3205e2a6638e6d750f53db8
you lee <letsgosmoke@gmail.com>
2021-12-10 18:32:24 +0800

    Разделяй и властвуй

---
+++ src/view/site-board-view.js
@@ -0,0 +1,9 @@
+import AbstractView from './abstract-view';
+
+const createBoardTemplate = () => ('<section class="films"><section class="films-list"><h2 class="films-list__title visually-hidden">All movies. Upcoming</h2><div class="films-list__container"></div></section></section>');
+
+export default class BoardView extends AbstractView {
+  get template() {
+    return createBoardTemplate();
+  }
+}
